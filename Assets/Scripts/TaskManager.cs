2fbab10bfd5e293ca9de640d950781c724b13f03
Tturna <42873350+Tturna@users.noreply.github.com>
2021-11-18 14:17:03 +0200

    fuck you git

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class TaskManager : MonoBehaviour
 {
+    [SerializeField] int tasksOnScreen;
+    [SerializeField] int tasksInGame;
+
+    // List of all possible tasks
+
+    // List of tasks to use for this game
+
     void Start()
     {
-        
+        // Populate used task list with random tasks
+        PopulateTaskList();
+
+        // Show top 5 in UI
+        UpdateUI();
     }
 
     void Update()
     {
         
+    }
+
+    void PopulateTaskList()
+    {
+        // Create new temporary list for easy random picking
+        //List<> all = allTasks;
+
+        //// Populate the tasks list with random tasks
+        //for (int i = 0; i < tasksInGame; i++)
+        //{
+        //    int r = Random.Range(0, all.Count); // Get random index
+        //    tasks.Add(all[r]); // Add task from temporary list
+        //    all.RemoveAt(r); // Remove task from temporary list to prevent duplicates
+        //}
+    }
+
+    void UpdateUI()
+    {
+
     }
 }
