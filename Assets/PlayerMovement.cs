b94d82412ae4eea56f1b553b84613850f3bd2c8e
Legends <harikrishnan.apps@gmail.com>
2021-11-18 17:31:18 +0530

    Implemented Player Animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,20 +7,44 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     Vector2 movement;
+    public Animator animator;
+    private bool isFacingRight;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movement = moveInput.normalized * moveSpeed;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if(moveInput.x > 0)
+        {
+            isFacingRight = true;
+        }
+        
+        if(moveInput.x < 0)
+        {
+            isFacingRight = false;
+        }
+
+        if(isFacingRight)
+        {
+            transform.localScale = new Vector3(1,1,1);
+        }else
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);      
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);      
     }
 }
