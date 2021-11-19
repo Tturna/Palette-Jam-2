8f759032eaeb7ee2f17d6e3b2af01180977cea75
Legends <harikrishnan.apps@gmail.com>
2021-11-19 22:40:15 +0530

    Stuff

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -30,6 +30,7 @@ public class InteractionManager : MonoBehaviour
         if (collision.gameObject.CompareTag("Interactable"))
         {
             nearObjects.Add(collision.gameObject);
+            Debug.Log(collision.gameObject.name);
         }
     }
 
